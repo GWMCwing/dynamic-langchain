57f731396887db5367ac80ecd6875c1e135f8cc4
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2024-02-13 20:29:25 +0800

    include database typing as type export

---
+++ packages/database/src/prisma-type/action.ts
@@ -0,0 +1,4 @@
+import { ChatSchema_Session } from "../database/chat/session.js";
+import { MethodReturnTypes } from "./helper.js";
+
+export type ChatSchema_Session_Action = MethodReturnTypes<ChatSchema_Session>;
